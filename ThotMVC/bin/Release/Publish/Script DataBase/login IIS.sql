772cbbda00329b1adcc8fede5a7d0e0e446f8d7b
JuanCarlosReyesGuerrero <ingreyesguerrero@gmail.com>
2018-12-07 16:07:00 -0500

    instaladores

---
+++ ThotMVC/bin/Release/Publish/Script DataBase/login IIS.sql
@@ -0,0 +1,10 @@
+CREATE LOGIN [IIS APPPOOL\DefaultAppPool] FROM WINDOWS
+GO
+--USE Table Name
+GO
+CREATE USER [IIS APPPOOL\DefaultAppPool] FOR LOGIN [IIS APPPOOL\DefaultAppPool]
+GO
+EXEC sp_addrolemember 'db_datareader', 'IIS APPPOOL\DefaultAppPool'
+GO
+EXEC sp_addrolemember 'db_datawriter', 'IIS APPPOOL\DefaultAppPool'
+GO
